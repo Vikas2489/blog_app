2bf8cd861a83ee56b3db9c2e40c1c2152a5ce857
vikas2489 <vk55002678@gmail.com>
2022-12-14 14:18:49 +0530

    update constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,5 @@
-// const rootURL = 'https://article-api.onrender.com/api/';
-const rootURL = 'http://localhost:1000/api/';
-// const articlesURL = 'https://article-api.onrender.com/api/articles';
-const articlesURL = 'http://localhost:1000/api/articles';
-const tagsURL = 'http://localhost:1000/api/tags';
+const rootURL = 'https://article-api.onrender.com/api/';
+const articlesURL = 'https://article-api.onrender.com/api/articles';
+const tagsURL = 'https://article-api.onrender.com/api/tags';
 
 export { rootURL, tagsURL, articlesURL };
